e2b5e803e63246ad8b79d65fbaed4cb0e5ca7582
Mike Ringrose <mikeringrose@gmail.com>
2012-06-08 09:26:39 -0400

    Cleaned up the code.

--- demo/demo.js
+++ demo/demo.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     var mapEl = document.getElementById('map'),
-        // center = new yarn.LatLng(0, 0),
         lat = 40.039722,
         lng = -76.304444,
         center = new yarn.LatLng(lat, lng),
-        // center = new yarn.LatLng(60, -130),
         options = {
             el: mapEl,
             center: center,
@@ -13,5 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
         },
         map = new yarn.Map(options);
 
-        setTimeout(function() { map.setZoom(8); }, 1500);
+        setTimeout(function() { 
+            map.setZoom(8); 
+        }, 1500);
 });
