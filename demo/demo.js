c5332ce2502755b4f05ff7f91e963f90908913a1
mike.ringrose@gmail.com <mikeringrose@gmail.com>
2012-10-28 12:47:10 -0400

    Switching to jquery document ready function.

--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(function() {
     var mapEl = document.getElementById('map'),
         center = new yarn.LatLng(40.039722, -76.304444),
         options = { el: mapEl, center: center, zoom: 16, tileSize: 256 },
