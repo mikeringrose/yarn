f95bac13015f8da14ecf2505d13581b5250b0599
mike.ringrose@gmail.com <mikeringrose@gmail.com>
2012-10-28 12:46:23 -0400

    Adding tile size.

--- demo/demo.js
+++ demo/demo.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     var mapEl = document.getElementById('map'),
         center = new yarn.LatLng(40.039722, -76.304444),
-        options = { el: mapEl, center: center, zoom: 16 },
+        options = { el: mapEl, center: center, zoom: 16, tileSize: 256 },
         map = new yarn.Map(options);
         
         map.addMarker({ latLng: center });
